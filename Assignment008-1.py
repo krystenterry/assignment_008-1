b6a3ea8493ba8668acbf3eb50c691f4cfb6e6231
Krysten Terry <krystenterry@gmail.com>
2021-04-23 17:57:36 -0400

    Assignment008-1

---
+++ Assignment008-1.py
@@ -0,0 +1,9 @@
+my_name = "Krysten"
+my_name2 = "krysten"
+your_name = input("Please enter your name : ")
+
+if my_name == your_name or my_name2 == your_name:
+  print('Hello', your_name, '! The password is : W@12')
+
+else:
+  print('Sorry ', your_name,'. See you later!')
