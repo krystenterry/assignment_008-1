d1fe40c8532d3dc28a1d15005077bd86925605d9
Krysten Terry <krystenterry@gmail.com>
2021-04-26 21:53:01 -0400

    Coronavirus code

---
+++ Coronavirus.py
@@ -0,0 +1,9 @@
+age = input('Answer yes or no to the following question : Are you a cigarette addict older than 75 years old? ')
+chronic = input('Answer yes or no to the following question : Do you have a severe chronic disease? ')
+immune = input('Answer yesr or no to the following question : Do you have a sever chronic disease? ')
+
+if age == yes or chronic == yes or immune == yes:
+    print('You are in a risky group.')
+
+else:
+    print('You are not in risky group.')
